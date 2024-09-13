3a34f2d4875f948966bb655b3fd7c5f31dffe06d
Ronny <fake@mail.com>
2024-09-13 19:57:08 -0300

    Unificação de sites e exercícios

--- js/site.js
+++ js/site.js
@@ -1,2 +1,8 @@
-
-
+$(document).ready(function(){
+    $('#btnSearch').click(function(){
+        let searchField = $("#iptSearch");
+        console.log(searchField);
+        console.log(searchField.value);
+        window.alert(searchField.value);
+    });
+});
---
+++ site2/js/site.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#btnSearch').click(function(){
+        window.alert("Calma! Aqui é só css e html xD");
+    });
+});
